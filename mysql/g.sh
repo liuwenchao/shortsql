e93c759c2b0d3400be2ecb4a7c198e863e51568a
Louis Liu <louis.wenchao.liu@gmail.com>
2013-09-29 13:16:18 +0800

    mv .my.cnf to my.cnf, and add more commands.

--- mysql/g.sh
+++ mysql/g.sh
@@ -13,7 +13,7 @@ source .my.cnf
 : ${user:="root"}
 : ${pass:="root"}
 : ${db:="test"}
-echo "login by: mysql $db -u$user -p$pass"
+echo "will login by: mysql $db -u$user -p$pass"
 
 #
 while read -p 'SQL> ' line
@@ -30,6 +30,8 @@ do
       table=${c[1]}
       param=${c[2]}
       case $op in
+        "d") sql="desc $table;";;
+        "s") sql="show create table $table;";;
         "l") sql="select * from $table;";;
         "c") sql="select count(*) from $table;";;
         "g") sql="select $param, count(*) from $table group by $param;";;
